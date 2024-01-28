8018f209b3cbac1a72876a7363fbba823a240eda
Ash Dey <ashabc [at] hotmail.com>
2024-01-29 10:05:58 +1100

    change from address

--- function_app.py
+++ function_app.py
@@ -165,7 +165,7 @@ def sendEmail(attachment):
                 }
             ]
         },
-        "senderAddress": "DoNotReply@9a80dbdd-e3ba-4c7d-9ffb-9a5e953179a6.azurecomm.net",
+        "senderAddress": "DoNotReply@d737b976-8c58-45e0-a0e7-4c16f5c097c4.azurecomm.net",
         "replyTo": [
             {
                 "address": "ash.dey@standards.org.au",  # Email address. Required.
